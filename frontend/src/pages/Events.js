4050cc682c2e4b8cd7c896050e3672fb46d25bdf
Mevlut <sevincmevlut@gmail.com>
2019-12-24 22:06:18 +0300

    #18 Bookings

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -162,7 +162,46 @@ class EventsPage extends Component {
     });
   };
 
-  bookEventHandler = () => {};
+  bookEventHandler = () => {
+    if (!this.context.token) {
+      this.setState({ selectedEvent: null });
+      return;
+    }
+
+    const requestBody = {
+      query: `
+            mutation {
+              bookEvent(eventId: "${this.state.selectedEvent._id}") {
+                _id
+                createdAt
+                updatedAt
+              }
+            }
+        `
+    };
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.context.token
+      },
+      body: JSON.stringify(requestBody)
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        console.log(resData);
+        this.setState({ selectedEvent: null });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   render() {
     return (
@@ -213,7 +252,7 @@ class EventsPage extends Component {
             canConfirm
             onCancel={this.modalCancelHandler}
             onConfirm={this.bookEventHandler}
-            confirmText='Book'
+            confirmText={this.context.token ? 'Book' : 'Confirm'}
           >
             <h1>{this.state.selectedEvent.title}</h1>
             <h2>
